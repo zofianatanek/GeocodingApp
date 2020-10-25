697416097b05d79d5befb753d7208139afb8767c
zofianatanek <zofia.natanek@gmail.com>
2020-10-25 18:35:08 +0100

    Add client application. Display users list. Enable cors

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UsersList from './components//UsersList';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to Geocoding App</h2>
+
+      <UsersList />
+    </div>
+  );
+}
+
+export default App;
