66c6f2885c7ce35c35b99ade5f707f1d3ba0f92e
zofianatanek <zofia.natanek@gmail.com>
2020-10-26 08:18:57 +0100

    Refactoring

--- server/routes/addUser.js
+++ server/routes/addUser.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const Joi = require('joi');
+const cors = require('cors');
 const { createUser, User, validate } = require('../models/user');
 
+router.options('*', cors());
+
 router.post('/', async (req, res) => {
-  const validation = validate(req.body);
+  const validation = await validate(req.body);
   const user = await User.findOne({ email: req.body.email });
   if (validation) {
-    res.status(400).send(validation.error.details[0].message);
+    return res.status(400).send(validation.error.details[0].message);
   } else if (user) {
     return res.status(400).send('Podany e-mail wystęuje w bazie');
   } else {
